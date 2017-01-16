9417a450920e52c52fd5bac8f3c13562894d6e67
Bunstance <Bunstance@outlook.com>
2017-01-16 19:29:08 +0000

    exper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
+
+  # is this visible on github
   
   def array_from_string string
     # takes the string found when params tries to return an array, and reconstruct the array.
