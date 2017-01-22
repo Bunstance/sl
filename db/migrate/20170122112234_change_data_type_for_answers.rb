c9ba84cec2ca19eff63a60bc5c3a24302aa41a12
Bunstance <Bunstance@outlook.com>
2017-01-22 11:58:09 +0000

    little things

--- db/migrate/20170122112234_change_data_type_for_answers.rb
+++
@@ -1,9 +0,0 @@
-class ChangeDataTypeForAnswers < ActiveRecord::Migration
-  def self.up
-    change_column :questions, :answers, :text
-  end
-
-  def self.down
-    change_column :questions, :answers, :string
-  end
-end
